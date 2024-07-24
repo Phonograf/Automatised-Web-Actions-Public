6b691ee84c6f8d714a14d0f7e86733c8bcc69502
Phonograf <goloviznin.danya@yandex.ru>
2024-07-25 00:12:30 +0200

    Fast fix of VPN

--- Browser/scripts/Windscribe.js
+++ Browser/scripts/Windscribe.js
@@ -1,13 +1,12 @@
 //Dedicated imports 
 import { config } from "dotenv";
-config( {path: `../../Configs/.env.client` });
+config( {path: `../Configs/.env.client` });
 import cmd from 'node-cmd';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 import log from './functions.js';
 import sqlite3 from 'better-sqlite3';
-//let DBSOURCE = `../${process.env.PathToDB}`;
 let DBSOURCE = process.env.PathToDB;
 let db = new sqlite3(DBSOURCE, {});
 
