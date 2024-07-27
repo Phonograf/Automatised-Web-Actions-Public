80ebbbe0b4f0004dec27ef75053130ad90824472
Phonograf <goloviznin.danya@yandex.ru>
2024-07-27 15:14:32 +0200

    bat folder fix

--- Useful tools/start_manual.bat
+++ Useful tools/start_manual.bat
@@ -1,6 +1,7 @@
 @echo off
 color 0a
 echo Manual calling
+cd ../Browser
 set /p "id=Enter ID: "
 echo Launching with %id%
 start node self.js manual %id%
--- Useful tools/start_recurrent.bat
+++ Useful tools/start_recurrent.bat
@@ -1,3 +1,4 @@
 @echo off
 color 0a
+cd ../Browser
 start node self.js recurrent
